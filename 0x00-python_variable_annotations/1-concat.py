dea42a715d74b9dac182f93a4f75f3ca5bf41113
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-07-06 02:41:29 +0100

    a function that returns a concatenated string

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,5 +1,5 @@
 def concat(str1: str, str2: str) -> str:
-    """takes a string str1 and a string str2 as arguments 
+    """takes a string str1 and a string str2 as arguments
     and returns a concatenated string
     """
     return str1 + str2
