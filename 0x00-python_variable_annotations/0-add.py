3047608bd034b8788818607adc06ea81451e548a
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-07-06 04:37:39 +0100

    adjusted the code to pycodestyle

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 def add(a: float, b: float) -> float:
     """This is returns the sum of 2 floats
     """
--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 def concat(str1: str, str2: str) -> str:
     """takes a string str1 and a string str2 as arguments
     and returns a concatenated string
